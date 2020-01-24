5e04e69bf9ac0b25539be5768d2738847e718dc5
kudayraju <kudayraju@gmail.com>
2020-01-24 16:51:10 +0530

    testing

--- test.js
+++ test.js
@@ -2,9 +2,9 @@ node () {
     stage('Build') { 
 		sh '''
 			export M2_HOME= "/opt/maven"
-    			export PATH= "${M2_HOME}/bin:${PATH}"
-			echo "PATH = ${PATH}"
-			echo "M2_HOME = ${M2_HOME}"
+    			//export PATH= "${M2_HOME}/bin:${PATH}"
+			//echo "PATH = ${PATH}"
+			//echo "M2_HOME = ${M2_HOME}"
 			//mvn clean package
 		'''
 	}
