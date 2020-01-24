b42df7dbfcf3874b78a4620b8f6bb96e99a9ffb8
kudayraju <kudayraju@gmail.com>
2020-01-24 16:25:09 +0530

    removed stages

--- test.js
+++ test.js
@@ -3,20 +3,18 @@ node () {
     M2_HOME= "/opt/maven"
     PATH= "${M2_HOME}/bin:${PATH}"
     }
-    stages {
-        stage('Build') { 
-            steps {
-                sh '''
-                    echo "PATH = ${PATH}"
-                    echo "M2_HOME = ${M2_HOME}"
-                    mvn clean package
-                '''
+	stage('Build') { 
+		steps {
+			sh '''
+				echo "PATH = ${PATH}"
+				echo "M2_HOME = ${M2_HOME}"
+				mvn clean package
+			'''
+		}
 	}
-        }
 	stage('Stage-Function-EG') { 
             uday()      
-        }    
-    }
+    }    
 }
 
 def uday() {
